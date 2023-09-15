4b4fc17b329db5c29a758c63dbbca4beb2fb3562
Jared P <jared.t.paden@gmail.com>
2023-09-15 14:12:36 -0600

    start btn functionality+hiding elements+alert

--- js/main.js
+++ js/main.js
@@ -1,16 +1,42 @@
+var questions = [
+    {
+        question: 'What does HTML stand for?',
+        answers: [
+            {text: 'Hyper Text Markup Language', correct: true},
+            {text: 'Hyper Technical Markup Language', correct: false }
+        ]
+    }
+]
+
+var randomQuestions
+var currentQuestion
 var timer = document.getElementById("timer")
 var startButton = document.getElementById("start")
+var questionContainerEl = document.getElementById("question-container");
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+
+}
 // specify where the timer starts at
 var timeLeft = 75;
-// declare a function to add logic to "start" button
+// add function for start button
 function startTest() {
+// stop hiding questions once test has started
+    questionContainerEl.classList.remove("hide");
+    startButton.classList.add("hide");
+// begin decreasing timer as test starts
 var timeInterval = setInterval(function () {
     timeLeft--;
-
-    timer.textContent = timeLeft + "seconds remaining";
+// display the time left on the timer element
+    timer.textContent = timeLeft + " seconds remaining";
 // when the time reaches 0, stop the timer
     if (timeLeft === 0) {
     clearInterval(timeInterval);
+    window.alert("Time is up! Your test is complete")
     }
     }, 1000);
 }
